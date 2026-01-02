8b7860e68b4e1eb72b02b8351faab904bbbef91d
Siti Nur Iman Nadhirah Binti Mohd Faizal <imancyan1709@gmail.com>
2026-01-02 23:29:56 +0800

    Update index.blade.php

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -325,6 +325,12 @@
                     <li class="nav-item">
                         <a class="nav-link nav-link-hasta" href="#footer-hasta">Contact</a>
                     </li>
+
+                    @auth
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-hasta" href="{{ route('profile.edit') }}">Profile</a>
+                    </li>
+                    @endauth
                 </ul>
 
                 <div class="d-flex align-items-center gap-3">
