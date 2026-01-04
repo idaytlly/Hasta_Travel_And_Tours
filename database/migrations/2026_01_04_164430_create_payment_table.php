d1a6d8c68ebffddc80d892510c9691eef8a73729
Azdayana Azahari <azdayanabatrisyia@gmail.com>
2026-01-05 01:27:57 +0800

    table migration

---
+++ database/migrations/2026_01_04_164430_create_payment_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->string('payment_id')->primary();
+
+            $table->double('amount')->nullable();
+            $table->mediumText('payment_status')->nullable();
+            $table->double('total_price')->nullable();
+            $table->double('deposit')->nullable();
+            $table->double('remaining_payment')->nullable();
+            $table->date('payment_date')->nullable();
+            $table->string('payment_proof')->nullable();
+
+            //FK
+            $table->string('booking_id')->nullable();
+            $table->foreign('booking_id')->references('booking_id')->on('booking');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment');
+    }
+};
---
+++ database/migrations/2026_01_04_171131_create_voucher_redemption_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voucher_redemption', function (Blueprint $table) {
+            $table->date('redemption_date')->nullable();
+
+            //FK
+            $table->string('matricNum')->nullable();
+            $table->foreign('matricNum')->references('matricNum')->on('customers');
+
+            $table->string('voucher_id')->nullable();
+            $table->foreign('voucher_id')->references('voucher_id')->on('voucher');
+
+            $table->primary(['matricNum', 'voucher_id'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voucher_redemption');
+    }
+};
