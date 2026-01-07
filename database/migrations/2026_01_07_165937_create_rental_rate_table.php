6a016431639edd4ffda4a6845c6d9b76bf6be1af
Azdayana Azahari <azdayanabatrisyia@gmail.com>
2026-01-08 02:02:00 +0800

    Create 2026_01_07_165937_create_rental_rate_table.php

---
+++ database/migrations/2026_01_07_165937_create_rental_rate_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rental_rate', function (Blueprint $table) {
+            $table->string('rate_id')->primary();
+
+            $table->integer('hours');
+            $table->decimal('rate_price', 10, 2);
+
+            $table->timestamps();
+
+            $table->string('plate_no')->nullable();
+            $table->foreign('plate_no')->references('plate_no')->on('vehicle');
+            
+            $table->string('staff_id')->nullable();
+            $table->foreign('staff_id')->references('staff_id')->on('staff');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rental_rate');
+    }
+};
