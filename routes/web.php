7907f6e3a60879d8d44a2c8266a5ecb865642aea
Siti Nur Iman Nadhirah Binti Mohd Faizal <imancyan1709@gmail.com>
2026-01-08 01:05:50 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('guest.home');
-})->name('guest.home');
+    Route::get('/', function () {
+        return view('guest.home');
+    })->name('guest.home');
 
 ?>
