0f7ddda5aac185f2eb5b808cf177c37079918787
Nur Alia Athirah Binti Suzuddin <aliaathirah005@gmail.com>
2026-01-11 03:59:03 +0800

    db

--- database/migrations/2026_01_06_153037_create_customers_table.php
+++ database/migrations/2026_01_06_153037_create_customers_table.php
@@ -9,32 +9,32 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->string('customer_id')->primary();
             $table->string('email')->unique();
             $table->string('password');
             
             // Personal Information
-            $table->string('name');
-            $table->string('ic_number')->unique(); // IC or Passport number
-            $table->string('phone_no');
+            $table->string('name')->nullable();
+            $table->string('ic_number')->unique()->nullable();// IC or Passport number
+            $table->string('phone_no')->nullable();
             
             // File Uploads (REQUIRED)
-            $table->string('ic_passport_image'); // Front of IC/Passport (Required)
-            $table->string('license_image'); // Driving License (Required)
+            $table->string('ic_passport_image')->nullable(); // Front of IC/Passport (Required)
+            $table->string('license_image')->nullable(); // Driving License (Required)
             $table->string('matric_card_image')->nullable(); // Matric Card (Optional)
             
             // Optional Matric Number
             $table->string('matricNum')->nullable()->unique();
             
             // License Information
-            $table->string('license_no');
-            $table->date('license_expiry');
-            
+            $table->string('license_no')->nullable();
+            $table->date('license_expiry')->nullable();
+
             // Emergency Contact (REQUIRED)
-            $table->string('emergency_phoneNo');
-            $table->string('emergency_name');
-            $table->string('emergency_relationship');
-            
+            $table->string('emergency_phoneNo')->nullable();
+            $table->string('emergency_name')->nullable();
+            $table->string('emergency_relationship')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
