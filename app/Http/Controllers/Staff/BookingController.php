c9eab68b1adadd4113da7b243cef810f3f03d2a4
Nur Alia Athirah Binti Suzuddin <aliaathirah005@gmail.com>
2026-01-13 02:53:43 +0800

    Update BookingController.php

--- app/Http/Controllers/Staff/BookingController.php
+++ app/Http/Controllers/Staff/BookingController.php
@@ -157,7 +157,7 @@ class BookingController extends Controller
                     $oldVehicle->update(['availability_status' => 'available']);
                 }
             }
-            added commitsna
+            added efaafasfas
 
             $vehicle = Vehicle::findOrFail($request->plate_no);
             
